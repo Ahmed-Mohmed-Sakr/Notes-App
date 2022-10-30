07b831eb150a61b83552d3f681ac9756d0cb8568
Ahmed Sakr <80043395+Ahmed-Mohmed-Sakr@users.noreply.github.com>
2022-10-30 14:22:36 +0200

    add edit page to can edit every individual note

--- notes-functions.js
+++ notes-functions.js
@@ -28,7 +28,7 @@ const saveNotes = function (notes) {
 /// generate the DOM structure for a note
 const generateNoteDOM = function (note) {
   const noteEl = document.createElement("div");
-  const textEl = document.createElement("span");
+  const textEl = document.createElement("a");
   const button = document.createElement("button");
 
   // setup the remove button
@@ -46,6 +46,7 @@ const generateNoteDOM = function (note) {
   } else {
     textEl.textContent = "Unnamed note";
   }
+  textEl.setAttribute("href", `/edit.html#${note.id}`);
   noteEl.appendChild(textEl);
 
   return noteEl;
