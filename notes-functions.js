9d3f7248dcc53171a17613a8f43874a6e9f287a9
Ahmed Sakr <80043395+Ahmed-Mohmed-Sakr@users.noreply.github.com>
2022-11-13 19:03:22 +0200

    active sort by last editing

--- notes-functions.js
+++ notes-functions.js
@@ -67,9 +67,19 @@ const sortNotes = function (notes, sortBy) {
   } else if (sortBy === "byCreated") {
     return notes.sort(function (a, b) {
       if (a.createdAt > b.createdAt) {
-        return 1;
-      } else if (a.createdAt < b.createdAt) {
         return -1;
+      } else if (a.createdAt < b.createdAt) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+  } else if (sortBy === "byAlpha") {
+    return notes.sort(function (a, b) {
+      if (a.title.toLowerCase() < b.title.toLowerCase()) {
+        return -1;
+      } else if (a.title.toLowerCase() > b.title.toLowerCase()) {
+        return 1;
       } else {
         return 0;
       }
